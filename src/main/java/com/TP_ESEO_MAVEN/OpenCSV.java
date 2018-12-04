22041d1b577cdf618bfc50bbb8b2b8c3c1b008d6
simonazar <simonazar@192.168.1.70>
2018-12-04 23:20:25 +0100

    Ajout latitude et longitude

--- src/main/java/com/TP_ESEO_MAVEN/OpenCSV.java
+++ src/main/java/com/TP_ESEO_MAVEN/OpenCSV.java
@@ -23,95 +23,75 @@ import java.util.List;
 
 public class OpenCSV {
 
-    /*public static void main(String[] args) throws IOException {
+	@SuppressWarnings("null")
+	public static void main(String[] args) throws IOException, SQLException {
 
-        String fileName = "src/main/java/laposte_hexasmal.csv";
+		// BDD
+		String url = "jdbc:mysql://127.0.0.1:8889/maven";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String utilisateur = "jack";
+		String motDePasse = "jack";
+		Connection connexion = null;
+		Statement statement = null;
+		ResultSet resultat = null;
 
-        try (FileInputStream fis = new FileInputStream(fileName);
-                InputStreamReader isr = new InputStreamReader(fis, 
-                        StandardCharsets.UTF_8);
-                CSVReader reader = new CSVReader(isr)) {
-            String[] nextLine;
+		/* Chargement du driver JDBC pour MySQL */
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			/* Gérer les éventuelles erreurs ici. */
+		}
 
-            while ((nextLine = reader.readNext()) != null) {
+		connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
+		statement = connexion.createStatement();
 
-                for (String e : nextLine) {
-                    System.out.format("%s ", e);
-                }
-            }//fin while next line
-        }//fin CSV reader
-    }//fin main*/
-    
-    
+		String fileName = "src/main/java/laposte_hexasmal.csv";
+		Path myPath = Paths.get(fileName);
 
-        public static void main(String[] args) throws IOException, SQLException {
-        	
-        	//BDD
-        	String url = "jdbc:mysql://127.0.0.1:8889/maven";
-        	String driver = "com.mysql.cj.jdbc.Driver";
-        	String utilisateur = "jack";
-        	String motDePasse = "jack";
-        	Connection connexion = null;
-            Statement statement = null;
-            ResultSet resultat = null;
-            
-    		/* Chargement du driver JDBC pour MySQL */
-    		try {
-    		    Class.forName( driver );
-    		} catch ( ClassNotFoundException e ) {
-    		    /* Gérer les éventuelles erreurs ici. */
-    		}
-            
-		    connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
-		    statement = connexion.createStatement();
+		try (BufferedReader br = Files.newBufferedReader(myPath, StandardCharsets.UTF_8)) {
+			HeaderColumnNameMappingStrategy<LaPoste> strategy = new HeaderColumnNameMappingStrategy<LaPoste>();
+			strategy.setType(LaPoste.class);
 
-            String fileName = "src/main/java/laposte_hexasmal.csv";
-            Path myPath = Paths.get(fileName);
+			CsvToBean<LaPoste> csvToBean = new CsvToBeanBuilder<LaPoste>(br).withType(LaPoste.class)
+					.withMappingStrategy(strategy).withSeparator(';').withIgnoreLeadingWhiteSpace(true).build();
 
-            try (BufferedReader br = Files.newBufferedReader(myPath,StandardCharsets.UTF_8)) {
-                HeaderColumnNameMappingStrategy<LaPoste> strategy = new HeaderColumnNameMappingStrategy<LaPoste>();
-                strategy.setType(LaPoste.class);
+			List<LaPoste> laposte = csvToBean.parse();
 
-                CsvToBean<LaPoste> csvToBean = new CsvToBeanBuilder<LaPoste>(br)
-                        .withType(LaPoste.class)
-                        .withMappingStrategy(strategy)
-                        .withSeparator(';')
-                        .withIgnoreLeadingWhiteSpace(true)
-                        .build();
+			String sql = "INSERT INTO ville_france(Code_commune_INSEE, Nom_commune, Code_postal, Libelle_acheminement, Ligne_5, Latitude, Longitude) VALUES (?,?,?,?,?,?,?)";
 
-                List<LaPoste> laposte = csvToBean.parse();
-                
-                String sql = "INSERT INTO ville_france(Code_commune_INSEE, Nom_commune, Code_postal, Libelle_acheminement, Ligne_5, Latitude, Longitude) VALUES (?,?,?,?,?,?,?)"; 
-                
-                
-                for(int i=0; i < laposte.size(); i++) {
-                	PreparedStatement ps = (PreparedStatement) connexion.prepareStatement(sql);
-                	ps.setString(1, laposte.get(i).getCode_commune_INSEE());
-                	ps.setString(2, laposte.get(i).getNom_commune());
-                	ps.setString(3, laposte.get(i).getCode_postal());
-                	ps.setString(4, laposte.get(i).getLibelle_acheminement());
-                	ps.setString(5, laposte.get(i).getLigne_5());
-                	ps.setString(6, "1555");
-                	ps.setString(7, "1558");
-                	ps.executeUpdate();
-                	}
-             
-                
-                connexion.close();
-                
-                //System.out.println(laposte.get(0));
-                
-               
-                
-                //insert dans la BDD
-                //
-                
-            }
-        }
-    
-    
-    
+			for (int i = 0; i < laposte.size(); i++) {
+				PreparedStatement ps = (PreparedStatement) connexion.prepareStatement(sql);
+				ps.setString(1, laposte.get(i).getCode_commune_INSEE());
+				ps.setString(2, laposte.get(i).getNom_commune());
+				ps.setString(3, laposte.get(i).getCode_postal());
+				ps.setString(4, laposte.get(i).getLibelle_acheminement());
+				ps.setString(5, laposte.get(i).getLigne_5());
+				
+				//coodonnées GPS
+				System.out.println(i);
+				
+				String coord = laposte.get(i).getCoordonnees_gps();
+				String[] coordArray = new String[2];
+				coordArray[0]="0";
+				coordArray[1]="0";
+				if(coord != null && !coord.isEmpty()) {
+					coordArray = coord.split(",");
+				}
+				String lat = coordArray[0];
+				String lng = coordArray[1];
+				ps.setString(6, lat);
+				ps.setString(7, lng);
+				ps.executeUpdate();	
+			}
+
+			connexion.close();
+
+			// System.out.println(laposte.get(0));
+
+			// insert dans la BDD
+			//
+
+		}
+	}
+
 }
-
-
-
